5db1d62d3926f1df7e08618936af30a89f4f9c3f
Daniele Barell <dan.barell@gmail.com>
2026-02-03 12:49:22 +0100

    matrix aspect ratio 1/1

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,10 @@ import Matrix from "./components/matrix/matrix";
 function App() {
   return (
     <>
-      <p>prova text main</p>
       <h1 className="text-main text-main--product-name">
         color matrix studio v0.04
       </h1>
-      <h6>styles</h6>
       <button className="text-main text-main--label">Confirm</button>
-      <p className="text-main text-main--message">
-        Ipse lorum sit ament oppossum eccetera
-      </p>
-      <span className="text-main text-main--tooltip">Luminance inversion</span>
-      <code className="text-code">0,234</code>
-      <code className="text-code text-code--12">0,234</code>
       <Matrix />
       {/**<!--
        * matrix component
