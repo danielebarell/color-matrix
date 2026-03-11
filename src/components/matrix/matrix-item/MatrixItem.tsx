c63b743768835e76a3c7740cde71de0844b16725
Daniele Barell <dan.barell@gmail.com>
2026-03-11 08:51:48 +0100

    matrix item calibration

--- src/components/matrix/matrix-item/MatrixItem.tsx
+++ src/components/matrix/matrix-item/MatrixItem.tsx
@@ -7,10 +7,15 @@ type MatrixItemProps = {
   onItemSelected: (position: ColorMatrixPosition) => void;
 };
 export default function MatrixItem(props: MatrixItemProps) {
+  function handleClick() {
+    if (props.selected) return;
+    props.onItemSelected(props.position);
+  }
+
   return (
     <li
       className={`${styles.cell} ${props.selected ? styles["cell--selected"] : ""}`}
-      onClick={() => props.onItemSelected(props.position)}
+      onClick={handleClick}
     >
       <span className={`${styles["cell-value"]} text-code text-code--14`}>
         {props.value}
