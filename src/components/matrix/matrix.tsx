98b44fd7bf5d1df582bce10d3a9fdb6d86dbe808
Daniele Barell <dan.barell@gmail.com>
2026-03-15 18:25:41 +0100

    Matrix graphical fine tune
    - componenti matix inseriti in una 'cornice' che evidenzia ne evidenzia il tipo

--- src/components/matrix/matrix.tsx
+++ src/components/matrix/matrix.tsx
@@ -24,16 +24,28 @@ export default function Matrix() {
   const [selectedPostion, setSelectedPosition] =
     useState<ColorMatrixPosition | null>(null);
   return (
-    <section className={styles["matrix-table"]}>
-      {colorComponents.map((value, index) => (
-        <MatrixItem
-          position={index as ColorMatrixPosition}
-          value={value}
-          key={index}
-          onItemSelected={handleItemSelected}
-          selected={selectedPostion === index}
-        />
-      ))}
+    <section className={styles["matrix-wrapper"]}>
+      <span></span>
+      <span className={`${styles["frame-w"]} ${styles["--red"]}`}></span>
+      <span className={`${styles["frame-w"]} ${styles["--green"]}`}></span>
+      <span className={`${styles["frame-w"]} ${styles["--blue"]}`}></span>
+      <span className={`${styles["frame-w"]} ${styles["--alpha"]}`}></span>
+      <span className={`${styles["frame-w"]} ${styles["--plus"]}`}></span>
+      <span className={`${styles["frame-h"]} ${styles["--red"]}`}></span>
+      <span className={`${styles["frame-h"]} ${styles["--green"]}`}></span>
+      <span className={`${styles["frame-h"]} ${styles["--blue"]}`}></span>
+      <span className={`${styles["frame-h"]} ${styles["--alpha"]}`}></span>
+      <div className={styles["matrix-table"]}>
+        {colorComponents.map((value, index) => (
+          <MatrixItem
+            position={index as ColorMatrixPosition}
+            value={value}
+            key={index}
+            onItemSelected={handleItemSelected}
+            selected={selectedPostion === index}
+          />
+        ))}
+      </div>
     </section>
   );
 }
